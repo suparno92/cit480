c6910c4e450afde2df0a1f2aa63ada186bedc6f5
Eliot Jordon <ejordon@gmail.com>
2017-01-31 09:30:54 -0800

    change connection code again
    I took out the connection code I put in last night.  I opted to use this setup from w3schools due to its ease of readability.

--- mysql_connection.php
+++ mysql_connection.php
@@ -1,5 +1,4 @@
 <?php
-
 // Original Code Commented out
 //---------------------------------------------------------------
 /*
@@ -18,53 +17,27 @@ $conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
 
     }
 */
+
+//---------------------------------------------------------------
+// I decided to go with a version from w3schools because it seems
+// quite readable.
 //---------------------------------------------------------------
 
 // New connection code using PDO object
-// Original connection.php script found here:
-//http://thisinterestsme.com/php-user-registration-form/
 
-/**
- * This script connects to MySQL using the PDO object.
- * This can be included in web pages where a database connection is needed.
- * Customize these to match your MySQL database connection details.
- * This info should be available from within your hosting panel.
- */
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-//User account info below will need to be changed
-
-//Our MySQL user account.
-define('MYSQL_USER', 'root');
-
-//Our MySQL password.
-define('MYSQL_PASSWORD', 'MyPass2016');
-
-//The server that MySQL is located on.
-define('MYSQL_HOST', 'localhost');
-
-//The name of our database.
-define('MYSQL_DATABASE', 'test');
-
-/**
- * PDO options / configuration details.
- * I'm going to set the error mode to "Exceptions".
- * I'm also going to turn off emulated prepared statements.
- */
-$pdoOptions = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false
-);
-
-/**
- * Connect to MySQL and instantiate the PDO object.
- */
-$pdo = new PDO(
-    "mysql:host=" . MYSQL_HOST . ";dbname=" . MYSQL_DATABASE, //DSN
-    MYSQL_USER, //Username
-    MYSQL_PASSWORD, //Password
-    $pdoOptions //Options
-);
-
-//The PDO object can now be used to query MySQL.
-
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
+  
